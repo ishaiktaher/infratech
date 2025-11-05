498abc4ef677077eb771a79821a342db571389ed
Shaik Taher <shaikt30@gmail.com>
2025-11-05 10:41:00 +0530

    Updated logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
-import { Building2 } from 'lucide-react';
+// import { Building2 } from 'lucide-react';
+import logo from '../assets/images/logo.png';
 
 export default function Footer() {
   return (
@@ -6,13 +7,15 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-6">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center gap-3 mb-6">
-            <div className="w-12 h-12 bg-amber-400 rounded-lg flex items-center justify-center">
-              <Building2 className="text-teal-800" size={28} />
+            <div className="bg-teal-800 h-12 bg-amber-400 rounded-lg flex items-center justify-center">
+              {/* <Building2 className="text-teal-800" size={28} /> */}
+          <img src={logo} alt="InfraTech Logo" className="h-16 w-auto" />
+
             </div>
-            <div className="text-left">
+            {/* <div className="text-left">
               <h3 className="text-2xl font-bold">InfraTech</h3>
               <p className="text-sm text-gray-300">Construction Materials</p>
-            </div>
+            </div> */}
           </div>
 
           <p className="text-gray-300 mb-6 max-w-md">
